425c060d9f72e59491864a8a2de76255f98ad31c
trongltgit <tronglt.hcm@gmail.com>
2026-04-12 12:35:02 +0700

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,7 +1,7 @@
 from groq import Groq
 import os
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY_BK"))
 
 def extract_data(text, url):
     prompt = f"""
